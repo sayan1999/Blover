0d95f5c67e89d1cfb6782ff6172c884b24930ab5
sayan1999 <mr.sayan.dey@gmail.com>
2023-08-02 20:10:46 +0530

    website changes

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Creating a Web server using Python and Flask
-
 from flask import Flask, render_template, request, make_response, jsonify, Response
 import io
 import base64
@@ -12,8 +10,18 @@ def b_summarize(text):
     BLOG().summarize(text)
 
 
-def b_createcover(pos, neg):
+def b_createcover(pos="", neg=""):
     COVER().create(pos=pos, neg=neg)
+    return Image.open(BLOVER.cover)
+
+
+def img_to_base64(img):
+    buffer = io.BytesIO()
+    img.save(buffer, "png")
+    buffer.seek(0)
+
+    data = buffer.read()
+    return base64.b64encode(data).decode()
 
 
 bloverapp = Flask("blover")
@@ -24,36 +32,12 @@ def run():
     return render_template("index.html")
 
 
-@bloverapp.route("/summarize", methods=["POST"])
-def summarize():
-    # print("Blog->>>>>>>>>", request.get_json()["blog"])
+@bloverapp.route("/generate", methods=["POST"])
+def generate():
     b_summarize(request.get_json()["blog"].strip())
-    return jsonify({"summary": BLOVER.summary})
-
-
-@bloverapp.route("/createcover", methods=["POST"])
-def createcover():
-    if not BLOVER.summary:
-        print("Summary missing, can't create cover")
-        img = Image.open("static/DpT093HUcAIH6D-.jpeg")
-    else:
-        pos = request.get_json()["pos"].strip()
-        neg = request.get_json()["neg"].strip()
-        b_createcover(pos=pos, neg=neg)
-        img = Image.open(BLOVER.cover)
-    # img = img.convert("L")  # ie. convert to grayscale
-
-    # data = file.stream.read()
-    # data = base64.b64encode(data).decode()
-
-    buffer = io.BytesIO()
-    img.save(buffer, "png")
-    buffer.seek(0)
-
-    data = buffer.read()
-    data = base64.b64encode(data).decode()
-    return data
-    # return f'<img src="data:image/png;base64,{data}">'
+    img = b_createcover()
+    width, height = img.size
+    return jsonify({"img": img_to_base64(img), "width": width, "height": height})
 
 
 if __name__ == "__main__":
